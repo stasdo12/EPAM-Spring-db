1227a29dbfa5db900224d392ea2e943bf5435c67
Stanislav Donetc <stanislavdonetc@gmail.com>
2024-11-13 17:45:46 +0200

    added a eureka

--- spring.core/src/main/java/com/epam/springcore/task/Application.java
+++ spring.core/src/main/java/com/epam/springcore/task/Application.java
@@ -1,8 +1,10 @@
 package com.epam.springcore.task;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(scanBasePackages = "com.epam.springcore")
+@EnableDiscoveryClient
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
