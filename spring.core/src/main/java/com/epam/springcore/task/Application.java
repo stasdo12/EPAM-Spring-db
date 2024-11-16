e37dd195f41ada4df0affd8ab5a477e1f091e27e
Stanislav Donetc <stanislavdonetc@gmail.com>
2024-11-16 10:56:18 +0200

    changed app prop, and microservice client

--- spring.core/src/main/java/com/epam/springcore/task/Application.java
+++ spring.core/src/main/java/com/epam/springcore/task/Application.java
@@ -1,13 +1,16 @@
 package com.epam.springcore.task;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 
 
 @SpringBootApplication(scanBasePackages = "com.epam.springcore")
 @EnableDiscoveryClient
-@EnableFeignClients(basePackages = "com.epam.springcore.task.client")
+@EnableFeignClients
+@ImportAutoConfiguration({FeignAutoConfiguration.class})
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
--- spring.core/src/main/java/com/epam/springcore/task/client/MicroserviceClient.java
+++ spring.core/src/main/java/com/epam/springcore/task/client/MicroserviceClient.java
@@ -2,13 +2,15 @@ package com.epam.springcore.task.client;
 
 import com.epam.springcore.task.dto.TrainingRequest;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-//@FeignClient(name = "EPAMT1MICROSERVICE", url = "${feign.client.config.default.url}", fallback = MicroserviceFallback.class)
+@FeignClient(name = "microservice", url = "http://localhost:8081/", fallback = MicroserviceFallback.class)
+@Component
 public interface MicroserviceClient {
-    @PostMapping("/Workloads")
+    @PostMapping("/training-work/accept")
     void actionTraining(@RequestBody TrainingRequest trainingRequest,
                         @RequestHeader("Transaction-ID") String transactionId,
                         @RequestHeader("Authorization") String Authorization);
